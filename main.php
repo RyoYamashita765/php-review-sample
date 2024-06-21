bc026fe1ff0b3dac1434ccce2c794f3917570b0e
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-21 18:53:28 +0900

    Add "for" blocks

--- main.php
+++ main.php
@@ -1,3 +1,15 @@
 <?php
 
 echo "Hello, World!";
+
+for ($i = 1; $i <= 10; $i++) {
+    echo 'i = ' . $i . PHP_EOL;
+}
+
+for ($j = 1; $j <= 10; $j++) {
+    echo 'j = ' . $j . PHP_EOL;
+}
+
+for ($k = 1; $k <= 10; $k++) {
+    echo 'k = ' . $k . PHP_EOL;
+}
