bb8abb8058ab860013571a20fe3b5f00da7ad78d
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-21 18:54:49 +0900

    Update "for" blocks

--- main.php
+++ main.php
@@ -6,10 +6,10 @@ for ($i = 1; $i <= 10; $i++) {
     echo 'i = ' . $i . PHP_EOL;
 }
 
-for ($j = 1; $j <= 10; $j++) {
-    echo 'j = ' . $j . PHP_EOL;
-}
-
 for ($k = 1; $k <= 10; $k++) {
     echo 'k = ' . $k . PHP_EOL;
+}
+
+for ($m = 1; $m<= 10; $m++) {
+    echo 'm = ' . $m . PHP_EOL;
 }
