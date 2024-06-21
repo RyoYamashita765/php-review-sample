3f8e7ae719b6353f176f81d2167ab353727dcaa7
Ryo Yamashita <yamashita@ipride.co.jp>
2024-06-21 19:03:16 +0900

    Remove "for" blocks

--- main.php
+++ main.php
@@ -7,13 +7,5 @@ class SampleClass {
         for ($i = 1; $i <= 10; $i++) {
             echo 'i = ' . $i . PHP_EOL;
         }
-
-        for ($k = 1; $k <= 10; $k++) {
-            echo 'k = ' . $k . PHP_EOL;
-        }
-
-        for ($m = 1; $m<= 10; $m++) {
-            echo 'm = ' . $m . PHP_EOL;
-        }
     }
 }
